b12724ad2cbf3b6530442ed6007db73a64e180ba
jeongseonwoong <tjsdnd1219@naver.com>
2024-08-06 11:18:02 +0900

    logging and variable request mappin method

--- servlet/src/main/java/hello/servlet/web/frontcontroller/V5/FrontControllerServletV5.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/V5/FrontControllerServletV5.java
@@ -16,6 +16,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.ArrayList;
