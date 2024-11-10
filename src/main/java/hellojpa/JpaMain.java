e664f2d07e3a9ed47ff71f746c926d6f650028ba
jeongseonwoong <tjsdnd1219@naver.com>
2024-11-10 18:43:44 +0900

    many to mayny relationshio

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,13 +19,28 @@ public class JpaMain {
             entityManager.persist(team);
 
             Member member = new Member();
-            member.setUsername("member2");
+            member.setUsername("member1");
             member.setTeam(team);
             entityManager.persist(member);
 
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(team);
+            entityManager.persist(member2);
+
+            entityManager.flush();
+            entityManager.clear();
+
             Member findMember = entityManager.find(Member.class, member.getId());
-            Team team1 = findMember.getTeam();
-            System.out.println(team1);
+
+            List<Member> members = findMember.getTeam().getMembers();
+            System.out.println("===================");
+
+            for (Member member1 : members) {
+                System.out.println("member = " + member1.getTeam().getName());
+            }
+            System.out.println("===================");
+
             transaction.commit();
         } catch (Exception e){
             transaction.rollback();
