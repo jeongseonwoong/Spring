5438671fe6c24fd61b4b1ad069794dcc8b8756f4
jeongseonwoong <tjsdnd1219@naver.com>
2024-11-10 17:16:06 +0900

    one way relationship

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,34 +1,39 @@
-//package hellojpa;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.EntityTransaction;
-//import javax.persistence.Persistence;
-//import java.util.List;
-//
-//public class JpaMain {
-//    public static void main(String[] args) {
-//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
-//        EntityManager entityManager =entityManagerFactory.createEntityManager();
-//        //code
-//        EntityTransaction transaction = entityManager.getTransaction();
-//        transaction.begin();
-//        try{
-//            Member member = new Member();
-//            member.setUsername("kkk2");
-//            member.setRoleType(RoleType.ADMIN);
-//            member.setAge(13);
-//            entityManager.persist(member);
-//            System.out.println(entityManager.find(Member.class, member.getId()));
-//            System.out.println("==================");
-//            transaction.commit();
-//        } catch (Exception e){
-//            transaction.rollback();
-//        } finally{
-//            entityManager.close();
-//        }
-//
-//        entityManager.close();
-//        entityManagerFactory.close();
-//    }
-//}
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager =entityManagerFactory.createEntityManager();
+        //code
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        try{
+            Team team = new Team();
+            team.setName("Team 2");
+            entityManager.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member2");
+            member.setTeam(team);
+            entityManager.persist(member);
+
+            Member findMember = entityManager.find(Member.class, member.getId());
+            Team team1 = findMember.getTeam();
+            System.out.println(team1);
+            transaction.commit();
+        } catch (Exception e){
+            transaction.rollback();
+        } finally{
+            entityManager.close();
+        }
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,37 +1,29 @@
-//package hellojpa;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//import lombok.ToString;
-//
-//import javax.persistence.*;
-//import java.util.Date;
-//
-//@Entity
-//@TableGenerator(name = "MEMBER_SEQ_GENERATOR",
-//        table = "MY_SEQUENCES",
-//        pkColumnValue = "MEMBER_SEQ", allocationSize = 50)
-//@Setter
-//@Getter
-//@ToString
-//public class Member {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="member_seq_generator")
-//    private Long id;
-//    @Column(name = "name")
-//    private String username;
-//    private Integer age;
-//    @Enumerated(EnumType.STRING)
-//    private RoleType roleType;
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date createdDate;
-//    @Temporal(TemporalType.TIMESTAMP)
-//    private Date lastModifiedDate;
-//    @Lob
-//    private String description;
-//    //Getter, Setter…
-//    public Member() {
-//
-//    }
-//
-//}
+package hellojpa;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Setter
+@Getter
+@ToString
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+//    @Column(name= "TEAM_ID")
+//    private Long teamId;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+
+}
