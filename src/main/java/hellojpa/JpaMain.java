085c223b04cc24b5ef75022af747992ca9fc6b5a
jeongseonwoong <tjsdnd1219@naver.com>
2024-11-10 16:20:04 +0900

    example

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,34 +1,34 @@
-package hellojpa;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.util.List;
-
-public class JpaMain {
-    public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
-        EntityManager entityManager =entityManagerFactory.createEntityManager();
-        //code
-        EntityTransaction transaction = entityManager.getTransaction();
-        transaction.begin();
-        try{
-            //영속
-            Member member = entityManager.find(Member.class, 3L);
-            member.setUsername("d");
-            member.setAge(22);
-            member.setRoleType(RoleType.ADMIN);
-            transaction.commit();
-        } catch (Exception e){
-            transaction.rollback();
-        } finally{
-            entityManager.close();
-        }
-
-
-        entityManager.close();
-        entityManagerFactory.close();
-
-    }
-}
+//package hellojpa;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.EntityManagerFactory;
+//import javax.persistence.EntityTransaction;
+//import javax.persistence.Persistence;
+//import java.util.List;
+//
+//public class JpaMain {
+//    public static void main(String[] args) {
+//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+//        EntityManager entityManager =entityManagerFactory.createEntityManager();
+//        //code
+//        EntityTransaction transaction = entityManager.getTransaction();
+//        transaction.begin();
+//        try{
+//            Member member = new Member();
+//            member.setUsername("kkk2");
+//            member.setRoleType(RoleType.ADMIN);
+//            member.setAge(13);
+//            entityManager.persist(member);
+//            System.out.println(entityManager.find(Member.class, member.getId()));
+//            System.out.println("==================");
+//            transaction.commit();
+//        } catch (Exception e){
+//            transaction.rollback();
+//        } finally{
+//            entityManager.close();
+//        }
+//
+//        entityManager.close();
+//        entityManagerFactory.close();
+//    }
+//}
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,31 +1,37 @@
-package hellojpa;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
-import java.util.Date;
-
-@Entity
-@Setter
-@Getter
-public class Member {
-    @Id
-    private Long id;
-    @Column(name = "name")
-    private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob
-    private String description;
-    //Getter, Setter…
-    public Member() {
-
-    }
-
-}
+//package hellojpa;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//import javax.persistence.*;
+//import java.util.Date;
+//
+//@Entity
+//@TableGenerator(name = "MEMBER_SEQ_GENERATOR",
+//        table = "MY_SEQUENCES",
+//        pkColumnValue = "MEMBER_SEQ", allocationSize = 50)
+//@Setter
+//@Getter
+//@ToString
+//public class Member {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="member_seq_generator")
+//    private Long id;
+//    @Column(name = "name")
+//    private String username;
+//    private Integer age;
+//    @Enumerated(EnumType.STRING)
+//    private RoleType roleType;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//    @Lob
+//    private String description;
+//    //Getter, Setter…
+//    public Member() {
+//
+//    }
+//
+//}
