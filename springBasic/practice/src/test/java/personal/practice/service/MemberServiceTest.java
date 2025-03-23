03bdc943bc249a206f777eee024ebbfa0126a62e
jeongseonwoong <tjsdnd1219@naver.com>
2025-03-23 21:58:21 +0900

    feat :oauth 설정

---
+++ springBasic/practice/src/test/java/personal/practice/service/MemberServiceTest.java
@@ -0,0 +1,61 @@
+package personal.practice.service;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import personal.practice.domain.Member;
+import personal.practice.repository.MemoryMemberRepository;
+
+
+
+class MemberServiceTest {
+
+    MemberService memberService;
+
+
+    @BeforeEach
+    public void setUp()
+    {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        Long saveId=memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 중복회원예외(){
+        Member member = new Member();
+        member.setName("hello");
+        Member member1=new Member();
+        member1.setName("hello");
+        Long saveId=memberService.join(member);
+        try {
+            Long saveId1=memberService.join(member1);
+        }
+        catch (IllegalStateException e) {
+           assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+        }
+
+    }
+
+    @Test
+    void findMember() {
+    }
+
+    @Test
+    void findMemberById() {
+    }
+
+}
