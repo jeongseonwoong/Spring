aea50a2b9c1e1603d0d1e49f2476ceba4d5b319b
jeongseonwoong <tjsdnd1219@naver.com>
2025-03-23 21:29:28 +0900

    feat: 특정 url은 인가 없이 접근 가능하도록 설정, 나머지는 인가 필요하도록 설정, 로그아웃 시 로직 설정

--- OAuth/demo/src/main/java/com/oAuth2/demo/DemoApplication.java
+++ OAuth/demo/src/main/java/com/oAuth2/demo/DemoApplication.java
@@ -2,8 +2,16 @@ package com.oAuth2.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +22,30 @@ import java.util.Map;
 @RestController
 public class DemoApplication {
 
-	@GetMapping("/user")
-	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal){
-        return Collections.singletonMap("name",principal.getAttribute("name"));
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
     }
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/","/error","/webjars/**","/index.html").permitAll()
+				.anyRequest().authenticated()
+		).exceptionHandling(e -> e
+				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+		).csrf(c -> c
+						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+		)
+		.logout(l -> l
+						.logoutSuccessUrl("/").permitAll()
+		)
+		.oauth2Login(Customizer.withDefaults());
+
+		return http.build();
+	}
 }
