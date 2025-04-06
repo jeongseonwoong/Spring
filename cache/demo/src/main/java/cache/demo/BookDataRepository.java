11e33b05a3ac14ce55494ecb41012bd90374ef59
jeongseonwoong <tjsdnd1219@naver.com>
2025-04-06 21:38:39 +0900

    refactor: 메모리 -> db로 조회 변경

--- cache/demo/src/main/java/cache/demo/BookDataRepository.java
+++ cache/demo/src/main/java/cache/demo/BookDataRepository.java
@@ -1,2 +1,26 @@
-package cache.demo;public class BookDataRepository {
+package cache.demo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@RequiredArgsConstructor
+public class BookDataRepository implements BookRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public String getTitleByIsbn(String isbn) {
+        return jdbcTemplate.queryForObject("select title from Book where isbn = ?", String.class, isbn);
+    }
+
+    @Override
+    @Cacheable("books")
+    public String getTitleByIsbnUsingCache(String isbn) {
+        return jdbcTemplate.queryForObject("select title from Book where isbn = ?", String.class, isbn);
+    }
+
 }
