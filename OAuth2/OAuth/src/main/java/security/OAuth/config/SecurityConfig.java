76eaa3fd781765ebc768fd31ad76a47e9249bc94
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-02 20:43:28 +0900

    @EnableGlobalMethodSecurity 사용해보기

--- OAuth2/OAuth/src/main/java/security/OAuth/config/SecurityConfig.java
+++ OAuth2/OAuth/src/main/java/security/OAuth/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package security.OAuth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity // 활성화 <- 우리가 작성할 스프링 시큐리티 필터가 스프링 필터 체인에 등록이 된다.
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) //secured 어노테이션 활성화
 public class SecurityConfig{
 
     @Bean
@@ -29,7 +31,7 @@ public class SecurityConfig{
                         org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer::disable
                 ).logout(l -> l
                         .logoutUrl("/logout") //로그아웃 처리 url(기본값은 POST /logout)
-                        .logoutSuccessUrl("/login?logout") //로그아웃 성공 시 리다이렉트 할 경로
+                        .logoutSuccessUrl("/loginForm") //로그아웃 성공 시 리다이렉트 할 경로
                         .invalidateHttpSession(true) //세션 무효화
                         .clearAuthentication(true) //Authentication 객체 클리어
                         .deleteCookies("JSESSIONID") //삭제할 쿠키 이름들
@@ -39,7 +41,6 @@ public class SecurityConfig{
                 .loginProcessingUrl("/login") //
                 .defaultSuccessUrl("/")
                 ); // 권한이 없는 페이지에 access를 요청할 때 로그인 페이지로 이동하도록 하기.
-
 
         return http.build();
     }
