b9429d8e1169060f17ba349a7868c2d79f29b8ab
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-02 19:51:26 +0900

    feat: Spring DATA JPA 사용

--- OAuth2/OAuth/src/main/java/security/OAuth/repository/UserRepository.java
+++ OAuth2/OAuth/src/main/java/security/OAuth/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package security.OAuth.repository;public interface UserRepository {
+package security.OAuth.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import security.OAuth.Entity.User;
+
+//@Repository 어노테이션이 없어도 IOC됨. JpaRepository를 상속했기 때문에.
+public interface UserRepository extends JpaRepository<User,Integer> {
+    public User findByUsername(String username);
 }
