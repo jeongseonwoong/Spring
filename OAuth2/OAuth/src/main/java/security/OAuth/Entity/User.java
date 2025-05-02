f47a3cbadce92f4430bba72fc8e1983c7e1fc5a7
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-02 17:42:34 +0900

    feat: 사용자 객체 생성

--- OAuth2/OAuth/src/main/java/security/OAuth/Entity/User.java
+++ OAuth2/OAuth/src/main/java/security/OAuth/Entity/User.java
@@ -1,2 +1,47 @@
-package security.OAuth.Entity;public class User {
+package security.OAuth.Entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import security.OAuth.dto.UserForm;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id @GeneratedValue
+    private UUID id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    public void assignRole(Role role){
+        this.role = role;
+    }
+
+    public void bcryptPassword(String bCryptedPassword){
+        this.password = bCryptedPassword;
+    }
+
+    public User(UserForm userForm){
+        this.password = userForm.getPassword();
+        this.email = userForm.getEmail();
+        this.username = userForm.getUsername();
+    }
+
 }
