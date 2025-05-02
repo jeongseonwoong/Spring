1763ce8556dde3606a7ac343d8ef980b0d1700b2
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-02 19:49:56 +0900

    feat: 로그인을 위한 DTO class 추가

--- OAuth2/OAuth/src/main/java/security/OAuth/dto/UserForm.java
+++ OAuth2/OAuth/src/main/java/security/OAuth/dto/UserForm.java
@@ -1,2 +1,12 @@
-package security.OAuth.dto;public class UserDto {
+package security.OAuth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserForm {
+    private String username;
+    private String password;
+    private String email;
 }
