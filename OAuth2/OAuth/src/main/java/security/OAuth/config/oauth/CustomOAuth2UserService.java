dbec06e7475bfac628a20e98b9c78ed2fc6fe4d7
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-07 23:37:59 +0900

    refactor: oauth로그인 방식에 따라 정보를 저장하는 방식을 바꾼 후 로그인 하도록 변경

--- OAuth2/OAuth/src/main/java/security/OAuth/config/oauth/CustomOAuth2UserService.java
+++ OAuth2/OAuth/src/main/java/security/OAuth/config/oauth/CustomOAuth2UserService.java
@@ -11,7 +11,13 @@ import org.springframework.stereotype.Service;
 import security.OAuth.Entity.Role;
 import security.OAuth.Entity.User;
 import security.OAuth.config.CustomUserDetails;
+import security.OAuth.config.oauth.provider.FacebookUserInfo;
+import security.OAuth.config.oauth.provider.GoogleUserInfo;
+import security.OAuth.config.oauth.provider.NaverUserInfo;
+import security.OAuth.config.oauth.provider.OAuth2UserInfo;
 import security.OAuth.repository.UserRepository;
+
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -25,15 +31,27 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final UserRepository userRepository;
 
     @Override
+    //해당함수 종료시 @AuthenticationPrincipal이 만들어짐.
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         // 구글 로그인 버튼 클릭 -> 구글 로그인 창 -> 로그인 완료 -> code를 리턴(OAuth-client라이브러리) -> AccessToken 요청
         // userRequest 정보 -> loadUser함수 호출 -> 구글로부터 회원 프로필을 받음
         if(userRequest==null) throw new RuntimeException();
-        log.info("userRequest.getAttributes: {}", super.loadUser(userRequest).getAttributes());
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        String provider = userRequest.getClientRegistration().getClientId(); // 구글이면 구글
-        String providerId = (String) oAuth2User.getAttributes().get("sub");
-        String email = (String) oAuth2User.getAttributes().get("email");
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if(userRequest.getClientRegistration().getRegistrationId().equals("google")){
+            log.info("구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")){
+            log.info("페이스북 로그인 요청");
+            oAuth2UserInfo = new FacebookUserInfo(oAuth2User.getAttributes());
+        }else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")){
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
+        }else{
+            throw new OAuth2AuthenticationException("구글과 페이스북 로그인만 지원합니다.");
+        }
+        String provider = oAuth2UserInfo.getProvider(); // 구글이면 구글
+        String providerId = oAuth2UserInfo.getProviderId();
+        String email = oAuth2UserInfo.getEmail();
         String username = provider+"-"+providerId; //google_10974285182916427686
         String password = bCryptPasswordEncoder.encode("가나다라");
         Role role = Role.ROLE_USER;
