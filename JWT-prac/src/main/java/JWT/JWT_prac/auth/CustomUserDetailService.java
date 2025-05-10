a609161bf46b8835261d1bfe9ac9fc8550c900d9
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-10 21:07:32 +0900

    feat: 로그인 요청 시 CustomUser에 로그인 데이터 저장

--- JWT-prac/src/main/java/JWT/JWT_prac/auth/CustomUserDetailService.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/auth/CustomUserDetailService.java
@@ -1,2 +1,26 @@
-package JWT.JWT_prac.auth;public class CustomUserDetailService {
+package JWT.JWT_prac.auth;
+
+import JWT.JWT_prac.entity.User;
+import JWT.JWT_prac.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// http://localhost:8080/login 요청 시 동작 x. -> 이거 때문에 .formLogin(AbstractHttpConfigurer::disable) //폼태그를 이용한 로그인 안씀
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        log.info(userEntity.getUsername());
+        return new CustomUserDetails(userEntity);
+    }
 }
--- JWT-prac/src/main/java/JWT/JWT_prac/auth/CustomUserDetails.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/auth/CustomUserDetails.java
@@ -1,2 +1,35 @@
-package JWT.JWT_prac.auth;public class CustomUserDetails {
+package JWT.JWT_prac.auth;
+
+import JWT.JWT_prac.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    public CustomUserDetails(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRole().toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
 }
