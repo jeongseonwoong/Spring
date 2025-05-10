1badc59b4aa0d01e24807ce2ac04f3f63dd80c58
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-10 21:08:26 +0900

    refactor: JwtAuthenticationFilter -> loginFil class 이름 변경

--- JWT-prac/src/main/java/JWT/JWT_prac/jwt/LoginFilter.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/jwt/LoginFilter.java
@@ -5,5 +5,5 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 //스프링 시큐리티에서 UsernamePasswordAuthenticationFilter가 있는데
 //이 필터는 /login을 요청해서 username, password를 post로 전송하면
 //UsernamePasswordAuthenticationFilter 이게 동작을함. 이거 때문에 지금은 동작안함. .formLogin(AbstractHttpConfigurer::disable) //폼태그를 이용한 로그인 안씀
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 }
