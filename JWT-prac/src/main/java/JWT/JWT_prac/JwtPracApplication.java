5469f137855152abc927e2c98441471a0213eddf
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-10 15:53:28 +0900

    feat: JWT 공부용 repo 생성

---
+++ JWT-prac/src/main/java/JWT/JWT_prac/JwtPracApplication.java
@@ -0,0 +1,13 @@
+package JWT.JWT_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtPracApplication.class, args);
+	}
+
+}
---
+++ JWT-prac/src/test/java/JWT/JWT_prac/JwtPracApplicationTests.java
@@ -0,0 +1,13 @@
+package JWT.JWT_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
