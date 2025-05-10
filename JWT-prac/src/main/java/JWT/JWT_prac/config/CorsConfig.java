55eff188ff6dad8b8ca3929aa4fa2ad1a54ab2be
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-10 19:00:37 +0900

    feat: cors 처리 방식 설정 Bean 등록

--- JWT-prac/src/main/java/JWT/JWT_prac/config/CorsConfig.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package JWT.JWT_prac.config;
 
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,5 +20,18 @@ public class CorsConfig {
         config.addAllowedMethod("*"); //모든 Post, Get, Put, delete, patch 요청을 허용
         source.registerCorsConfiguration("/api/**",config);
         return new CorsFilter(source);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsSource(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); //내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정하는 것
+        config.addAllowedOriginPattern("*"); //모든 ip에 응답을 허용
+        config.addAllowedHeader("*"); // 모든 header에 응답을 허용
+        config.addAllowedMethod("*"); //모든 Post, Get, Put, delete, patch 요청을 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**",config);
+        return source;
     }
 }
