28f2a10e669e97e26c1eaede8a791e3b69a14669
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-10 21:08:57 +0900

    feat: Spring DATA JPA 사용해서 userRepository 설정

--- JWT-prac/src/main/java/JWT/JWT_prac/repository/UserRepository.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/repository/UserRepository.java
@@ -1,2 +1,8 @@
-package JWT.JWT_prac.repository;public class UserRepository {
+package JWT.JWT_prac.repository;
+
+import JWT.JWT_prac.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByUsername(String username);
 }
