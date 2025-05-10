783ed2343b2b9b16726664ae846d1682c7b544b5
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-10 19:38:15 +0900

    feat: example용 filter

--- JWT-prac/src/main/java/JWT/JWT_prac/filter/MyFilter1.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/filter/MyFilter1.java
@@ -1,2 +1,17 @@
-package JWT.JWT_prac.filter;public class MyFilter1 {
+package JWT.JWT_prac.filter;
+
+import jakarta.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+
+@Slf4j
+public class MyFilter1 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("필터1");
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
 }
--- JWT-prac/src/main/java/JWT/JWT_prac/filter/MyFilter2.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/filter/MyFilter2.java
@@ -1,2 +1,16 @@
-package JWT.JWT_prac.filter;public class MyFilter2 {
+package JWT.JWT_prac.filter;
+
+import jakarta.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class MyFilter2 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("필터2");
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
 }
