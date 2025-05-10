2046a72d94ba05e03b7560ecd73ebee445772ba2
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-10 19:37:51 +0900

    feat: JWT 이용을 위한 SecurityConfig

--- JWT-prac/src/main/java/JWT/JWT_prac/config/SecurityConfig.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/config/SecurityConfig.java
@@ -1,2 +1,43 @@
-package JWT.JWT_prac.config;public class SecurityConfig {
+package JWT.JWT_prac.config;
+
+import JWT.JWT_prac.filter.MyFilter1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
+    private final CorsConfigurationSource corsSource;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable) //csrf 비활성화
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //세션 안씀
+            .formLogin(AbstractHttpConfigurer::disable) //폼태그를 이용한 로그인 안씀
+            .httpBasic(AbstractHttpConfigurer::disable) // http 로그인 방식 안씀
+//            .addFilter(corsFilter) //@CrossOrigin(인증 x), 시큐리티 필터에 등록 인증(o)
+            .cors(cors-> cors.configurationSource(corsSource))
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/api/v1/user/**").hasAnyRole("USER","MANAGER","ADMIN")
+                    .requestMatchers("/api/1/manager/**").hasAnyRole("MANAGER","ADMIN")
+                    .requestMatchers("/api/1/admin/**").hasRole("ADMIN")
+                    .anyRequest().permitAll());
+        return http.build();
+    }
+
 }
