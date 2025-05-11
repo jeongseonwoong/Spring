25d7791c6cbcc4a73df0395b2ee07d59e10313e1
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-12 00:38:10 +0900

    feat: authentication 시도 함수

--- JWT-prac/src/main/java/JWT/JWT_prac/filter/jwt/LoginFilter.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/filter/jwt/LoginFilter.java
@@ -1,29 +1,79 @@
-package JWT.JWT_prac.jwt;
+package JWT.JWT_prac.filter.jwt;
 
+import JWT.JWT_prac.auth.CustomUserDetails;
+import JWT.JWT_prac.dto.LoginRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 //스프링 시큐리티에서 UsernamePasswordAuthenticationFilter가 있는데
 //이 필터는 /login을 요청해서 username, password를 post로 전송하면
 //UsernamePasswordAuthenticationFilter 이게 동작을함. 이거 때문에 지금은 동작안함. .formLogin(AbstractHttpConfigurer::disable) //폼태그를 이용한 로그인 안씀
 @RequiredArgsConstructor
+@Slf4j
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
-    private final AuthenticationManager authenticationManager;
-
     //login 요청을 하면 로그인 시도를 위해서 실행되는 함수
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        //1. username, password 받아서
+        if (!request.getMethod().equals("POST")) {
+            throw new AuthenticationServiceException(
+                    "Authentication method not supported: " + request.getMethod());
+        }
+
+        LoginRequest loginRequest;
+        try {
+            //1. username, password 받아서
+            loginRequest = new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        log.info("user= {},{}", loginRequest.getUsername(), loginRequest.getPassword());
+        String username = loginRequest.getUsername();;
+        String password = loginRequest.getPassword();
+
+        if (username == null) {
+            username = "";
+        }
+
+        if (password == null) {
+            password = "";
+        }
+
+        username = username.trim();
+
+        //토큰 생성
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
+
+        setDetails(request,authRequest);
         //2. 정상인지 로그인 시도를 해본다. authenticationManager로 로그인 시도를 하면 CustomUserDetailsService가 호출.
         //3. 그럼 loadUserByUsername()이 실행됨.
-        //4. 정상적으로 CustomUserDetails가 리턴이 되면 CustomUserDetails를 세션에 담고 (세션에 담는 이유, 권한 관리 USER, MANAGER, ADMIN을 위해서, JWT에 권한 넣어주면 안해줘도 된다네요)
+        // this.getAuthenticationManager().authenticate(authRequest)가 실행되면서 CustomUserService의 loadUserBysername()함수가 실행됨.
+        Authentication authentication = this.getAuthenticationManager().authenticate(authRequest);
+
         //5. 마지막으로 JWT 토큰을 만들어서 응답해주면됨.
-        return super.attemptAuthentication(request, response);
+        return authentication;
+    }
+
+
+    //attemptAuthentication이 정상적으로 실행되었을 때 실행. 여기서 JWT 토큰을 만들어서 response;
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        log.info("성공");
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
