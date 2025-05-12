df537e80b5547a1a2c356affaf5d385ef569cbb5
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-12 17:42:27 +0900

    feat: 토큰 verify 하는 필터

--- JWT-prac/src/main/java/JWT/JWT_prac/filter/TokenAuthenticationFilter.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/filter/TokenAuthenticationFilter.java
@@ -1,29 +1,63 @@
 package JWT.JWT_prac.filter;
 
-import jakarta.servlet.*;
+import JWT.JWT_prac.auth.CustomUserDetailService;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.security.sasl.AuthenticationException;
 import java.io.IOException;
 
+//시큐리티 필터중 BasicAuthenticationFilter는 권한이나 인증이 필요한 특정 주ㅗ를 요청했을 때 실행됨.
 @Slf4j
-public class TokenAuthenticationFIlter implements Filter {
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException, AuthenticationException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        //id, pw가 정상적으로 들어와서 로그인이 완료 되면 토큰을 만들어주고 그걸 응답으로 준다.
-        //이후, 요청할 때마다 header에 Authorization에 value값으로 토큰을 가져오면 가져온 토큰을 validate(RSA, HS256)해준다.
-        if(request.getMethod().equals("POST")){
-            log.info("POST 요청됨");
-            String headerAuth = request.getHeader("Authorization");
-            log.info(headerAuth);
 
-            if(!headerAuth.equals("token"))
-                throw new AuthenticationException("토큰인증이 안됐습니다.");
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+
+    //인증이나 권한이 필요한 주소요청이 있을 때 해당 필터를 타게 됨.
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("인증이나 권한이 필요한 주소가 요청됨.");
+
+        String jwtHeader = request.getHeader("Authorization");
+        log.info("jwtHeader={}",jwtHeader);
+
+        //JWT 토큰을 검증을 해서 정상적인 사용자인지 확인
+        if(jwtHeader == null || !jwtHeader.startsWith("Bearer")){
+            chain.doFilter(request,response);
+            return;
         }
-        filterChain.doFilter(request,response);
+
+        String jwtToken = request.getHeader("Authorization").replace("Bearer ","");
+
+        String username = JWT.require(Algorithm.HMAC512("abcd")).build().verify(jwtToken).getClaim("username").asString();
+
+        if(username!=null){
+            //서명이 정상적으로 이루어졌을 때
+            UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            //시큐리티의 context에 AuthenticationToken 저장
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+        chain.doFilter(request,response);
     }
 }
