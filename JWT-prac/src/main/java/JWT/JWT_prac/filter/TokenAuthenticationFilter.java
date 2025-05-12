4916a94c69037e6bee6aa74ee25a9f9cb2558b5b
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-12 17:45:05 +0900

    refactor: 코드 예쁘게 만들기

--- JWT-prac/src/main/java/JWT/JWT_prac/filter/TokenAuthenticationFilter.java
+++ JWT-prac/src/main/java/JWT/JWT_prac/filter/TokenAuthenticationFilter.java
@@ -9,14 +9,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -36,28 +32,28 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
         log.info("인증이나 권한이 필요한 주소가 요청됨.");
 
         String jwtHeader = request.getHeader("Authorization");
-        log.info("jwtHeader={}",jwtHeader);
+        log.info("jwtHeader={}", jwtHeader);
 
         //JWT 토큰을 검증을 해서 정상적인 사용자인지 확인
-        if(jwtHeader == null || !jwtHeader.startsWith("Bearer")){
-            chain.doFilter(request,response);
+        if (jwtHeader == null || !jwtHeader.startsWith("Bearer")) {
+            chain.doFilter(request, response);
             return;
         }
 
-        String jwtToken = request.getHeader("Authorization").replace("Bearer ","");
+        String jwtToken = request.getHeader("Authorization").replace("Bearer ", "");
 
         String username = JWT.require(Algorithm.HMAC512("abcd")).build().verify(jwtToken).getClaim("username").asString();
 
-        if(username!=null){
+        if (username != null) {
             //서명이 정상적으로 이루어졌을 때
             UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
             UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
             //시큐리티의 context에 AuthenticationToken 저장
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         }
-        chain.doFilter(request,response);
+        chain.doFilter(request, response);
     }
 }
