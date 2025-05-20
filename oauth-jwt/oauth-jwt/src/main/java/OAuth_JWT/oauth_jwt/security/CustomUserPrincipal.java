3f0d6c526c649347867e2380a2ae10a2a1a46f2d
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-20 14:32:50 +0900

    feat: 로그인 한 사용자의 정보를 담는 class

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/CustomUserPrincipal.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/CustomUserPrincipal.java
@@ -1,2 +1,62 @@
-package OAuth_JWT.oauth_jwt.security;public class CustomUserPrincipal {
+package OAuth_JWT.oauth_jwt.security;
+
+import OAuth_JWT.oauth_jwt.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class CustomUserPrincipal implements UserDetails, OAuth2User {
+
+    private User user;
+    private Map<String, Object> attributes;
+
+    CustomUserPrincipal(User user){
+        this.user =user;
+    }
+
+    public static CustomUserPrincipal create(User user){
+        return new CustomUserPrincipal(user);
+    }
+
+    public static CustomUserPrincipal create(User user, Map<String,Object> attributes){
+        CustomUserPrincipal customUserPrincipal = CustomUserPrincipal.create(user);
+        customUserPrincipal.setAttributes(attributes);
+        return customUserPrincipal;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getName() {
+        return user.getName();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
 }
