c5cf76827c463e01554b588f053156e61da2069e
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-20 20:00:20 +0900

    feat: oauth방식에 따라 저장 정보를 다르게 받도록 한다

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/oauth/OAuth2Classifier.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/oauth/OAuth2Classifier.java
@@ -1,2 +1,20 @@
-package OAuth_JWT.oauth_jwt.security.oauth;public class OAuth2Classifier {
+package OAuth_JWT.oauth_jwt.security.oauth;
+
+import java.util.Map;
+
+public class OAuth2Classifier {
+
+    private OAuth2Classifier(){};
+
+    public static OAuth2UserInfo classifyOAuth2Register(String registrationId, Map<String,Object> attributes ) throws IllegalAccessException {
+        if(registrationId.equalsIgnoreCase("google")){
+            return new GoogleOAuth2UserInfo(attributes);
+        }else if(registrationId.equalsIgnoreCase("facebook")){
+            return new FacebookOAuth2UserInfo(attributes);
+        }else if(registrationId.equalsIgnoreCase("naver")){
+            return new NaverOAuth2UserInfo(attributes);
+        }
+        throw new IllegalAccessException("구글, 페이스북, 네이버 소셜 로그인만 가능합니다.");
+    }
+
 }
