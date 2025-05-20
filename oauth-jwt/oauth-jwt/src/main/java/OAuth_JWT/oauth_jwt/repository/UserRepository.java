5e111dcb6062eefcb4ba752e94359c6ac56c812e
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-20 14:31:28 +0900

    feat: repository 인터페이스

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/repository/UserRepository.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package OAuth_JWT.oauth_jwt.repository;public interface UserRepository {
+package OAuth_JWT.oauth_jwt.repository;
+
+import OAuth_JWT.oauth_jwt.entity.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+    public User findByUsername(String username);
 }
