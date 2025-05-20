1eb61a4fda2f3144e5b24b1eb636d49ba6ab9371
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-20 14:32:28 +0900

    feat: 기본 로그인 시도 시 동작하는 메서드

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/auth/CustomUserDetailService.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/auth/CustomUserDetailService.java
@@ -1,2 +1,28 @@
-package OAuth_JWT.oauth_jwt.security.auth;public class CustomUserDetailService {
+package OAuth_JWT.oauth_jwt.security.auth;
+
+import OAuth_JWT.oauth_jwt.entity.User;
+import OAuth_JWT.oauth_jwt.repository.UserRepository;
+import OAuth_JWT.oauth_jwt.security.CustomUserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.file.attribute.UserPrincipal;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user==null) throw new UsernameNotFoundException("가입된 정보가 존재하지 않습니다.");
+        return CustomUserPrincipal.create(user);
+    }
 }
