73d2a3456205ec2a83f99c80dbb5ba4e59580788
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-21 15:17:21 +0900

    oauth 로그인 실패시 로그인 실패를 응답하도록 하는 클래스

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/handler/LoginFailureHandler.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/handler/LoginFailureHandler.java
@@ -1,2 +1,25 @@
-package OAuth_JWT.oauth_jwt.security.handler;public class LoginFaliureHandler {
+package OAuth_JWT.oauth_jwt.security.handler;
+
+import OAuth_JWT.oauth_jwt.dto.AuthResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LoginFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        new ObjectMapper().writeValue(response.getWriter(), "로그인 실패");
+    }
 }
