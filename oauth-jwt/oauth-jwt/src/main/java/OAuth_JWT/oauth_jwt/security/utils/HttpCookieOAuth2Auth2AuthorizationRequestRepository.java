1ca528026ecd635045181c0153bed31d30110714
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-21 15:24:21 +0900

    feat:쿠키이름으로 특정 쿠키를 찾고 redirect uri를 가져오는 기능을 재구성한 클래스

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/utils/HttpCookieOAuth2Auth2AuthorizationRequestRepository.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/utils/HttpCookieOAuth2Auth2AuthorizationRequestRepository.java
@@ -1,2 +1,67 @@
-package OAuth_JWT.oauth_jwt.security.utils;public class HttpCookieOAuth2Auth2AuthorizationRequestRepository {
+package OAuth_JWT.oauth_jwt.security.utils;
+
+import com.nimbusds.oauth2.sdk.util.StringUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.stereotype.Repository;
+
+@Repository
+/**
+ * Spring Security는 OAuth2 로그인 요청 시
+ * 내부적으로 AuthorizationRequestRepository를 사용해서 인증 요청 정보를 임시 저장해.
+ * 기본 구현체는 세션 기반이지만, 너처럼 **세션을 안 쓰는 구조(Stateless)**에서는 쿠키 기반 저장소를 만들어야 해.
+ * 그래서 직접 이 인터페이스를 구현하는 거지.
+ */
+public class HttpCookieOAuth2Auth2AuthorizationRequestRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
+
+    public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+    public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
+    private static final int COOKIE_EXPIRE_SECONDS = 180;
+
+    /**
+     * 쿠키에서 직렬화된 OAuth2AuthorizationRequest 객체를 꺼내서 복원
+     */
+    @Override
+    public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
+        return CookieUtils.getCookie(request,OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
+                .map(cookie -> CookieUtils.deserialize(cookie, OAuth2AuthorizationRequest.class))
+                .orElse(null);
+    }
+
+    /**
+     * 로그인 요청 정보를 쿠키에 저장
+     */
+    @Override
+    public void saveAuthorizationRequest(OAuth2AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
+        if(authorizationRequest == null){
+            CookieUtils.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+            CookieUtils.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
+            return;
+        }
+
+        CookieUtils.addCookie(response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME, CookieUtils.serialize(authorizationRequest),COOKIE_EXPIRE_SECONDS);
+        String redirectUriAfterLogin = request.getParameter(REDIRECT_URI_PARAM_COOKIE_NAME);
+        if (StringUtils.isNotBlank(redirectUriAfterLogin)) {
+            CookieUtils.addCookie(response, REDIRECT_URI_PARAM_COOKIE_NAME, redirectUriAfterLogin,
+                    COOKIE_EXPIRE_SECONDS);
+        }
+    }
+
+    /**
+     * 보통은 load와 같은 역할 (삭제는 별도로 처리)
+     */
+    @Override
+    public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request, HttpServletResponse response) {
+        return this.loadAuthorizationRequest(request);
+    }
+
+    /**
+     * 위 2개의 쿠키를 명시적으로 삭제
+     */
+    public void removeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response) {
+        CookieUtils.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+        CookieUtils.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
+    }
 }
