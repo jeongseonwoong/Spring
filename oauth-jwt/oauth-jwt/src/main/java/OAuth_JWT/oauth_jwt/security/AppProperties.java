112ba552a3fdc723cd72c679ff58958795f70f15
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-21 15:06:56 +0900

    feat: application.properites에서 설정한 값 가져오는 클래스

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/AppProperties.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/AppProperties.java
@@ -1,2 +1,33 @@
-package OAuth_JWT.oauth_jwt.security;public class AppProperties {
+package OAuth_JWT.oauth_jwt.security;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+    private final Auth auth = new Auth();
+    private final OAuth oauth = new OAuth();
+
+
+    public Auth getAuth() {return auth;}
+
+    public OAuth getOauth() {return oauth;}
+
+    @Getter
+    @Setter
+    public static class Auth {
+        private String tokenSecret;
+        private long tokenExpirationMsec;
+    }
+
+    @Getter
+    @Setter
+    public static class OAuth {
+        private List<String> authorizedRedirectUris = new ArrayList<>();
+    }
 }
