14f4b4be6d5909b33088bbab5f1d28e708d43ef7
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-21 15:22:19 +0900

    feat: 로그인 성공 시 쿠키에서 redirecturl 가져오고 토큰을 만들어 redirecturl로 토큰 전달

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/handler/OAuth2AuthenticationSuccessHandler.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,2 +1,77 @@
-package OAuth_JWT.oauth_jwt.security.handler;public class OAuth2AuthenticationSuccessHandler {
+package OAuth_JWT.oauth_jwt.security.handler;
+
+import OAuth_JWT.oauth_jwt.security.AppProperties;
+import OAuth_JWT.oauth_jwt.security.utils.CookieUtils;
+import OAuth_JWT.oauth_jwt.security.utils.TokenUtils;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.Optional;
+
+import static OAuth_JWT.oauth_jwt.security.utils.HttpCookieOAuth2Auth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final TokenUtils tokenUtils;
+
+    private final AppProperties appProperties;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        String targetUrl = determineTargetUrl(request, response, authentication);
+        if (response.isCommitted()) {
+            log.info("응답이 이미 전송되었습니다. {}", targetUrl);
+            log.info("로 리다이렉트가 불가능합니다.");
+        }
+
+        clearAuthenticationAttributes(request, response);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+    @Override
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue);
+
+        if (redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())) {
+            throw new RuntimeException("UNAUTHORIZED_REDIRECT_URI");
+        }
+
+        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+
+        String token = tokenUtils.createToken(authentication);
+
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("token", token)
+                .build().toUriString();
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+    }
+
+    private boolean isAuthorizedRedirectUri(String uri) {
+        URI clientRedirectUri = URI.create(uri);
+
+        return appProperties.getOauth().getAuthorizedRedirectUris()
+                .stream()
+                .anyMatch(authorizedRedirectUri -> {
+                    URI authorizedURI = URI.create(authorizedRedirectUri);
+                    return authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+                            && authorizedURI.getPort() == clientRedirectUri.getPort();
+                });
+    }
+
 }
