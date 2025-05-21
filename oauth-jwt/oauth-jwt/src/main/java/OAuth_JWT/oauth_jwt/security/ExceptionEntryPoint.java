72e1212e526a4f3985d4cc29b5fda08b94b489e2
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-21 15:08:27 +0900

    feat: 인증에 실패하였을 때 사용되는 exceptionEntrypoint

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/ExceptionEntryPoint.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/ExceptionEntryPoint.java
@@ -1,2 +1,17 @@
-package OAuth_JWT.oauth_jwt.security;public class ExceptionEntryPoint {
+package OAuth_JWT.oauth_jwt.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class ExceptionEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"인증이 실패하였습니다.");
+    }
 }
