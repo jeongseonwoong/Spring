ecb83936e1a4c7fbd695c76a02c98b154f3c498a
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-21 15:20:54 +0900

    feat: oauth 로그인 실패 시 url 에에러 메시지를 포함하여응답시키는 클래스

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/handler/OAuth2AuthenticationFailureHandler.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/handler/OAuth2AuthenticationFailureHandler.java
@@ -1,2 +1,39 @@
-package OAuth_JWT.oauth_jwt.security.handler;public class OAuth2AuthenticationFaliureHandler {
+package OAuth_JWT.oauth_jwt.security.handler;
+
+import OAuth_JWT.oauth_jwt.security.utils.CookieUtils;
+import OAuth_JWT.oauth_jwt.security.utils.HttpCookieOAuth2Auth2AuthorizationRequestRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+
+import static OAuth_JWT.oauth_jwt.security.utils.HttpCookieOAuth2Auth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+@RequiredArgsConstructor
+@Component
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    private final HttpCookieOAuth2Auth2AuthorizationRequestRepository httpCookieOAuth2Auth2AuthorizationRequestRepository;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        String targetUrl = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue)
+                .orElse(("/"));
+
+        targetUrl = UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("error", URLEncoder.encode(exception.getMessage(), "UTF-8"))
+                .build().toUriString();
+
+        httpCookieOAuth2Auth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
 }
