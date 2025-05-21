58603138b25f0fe6c793eacfcb85d2459646e77f
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-21 15:19:10 +0900

    feat: 로그인 성공 시 token을 응답하도록 하는 클래스

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/handler/LoginSuccessHandler.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/handler/LoginSuccessHandler.java
@@ -1,2 +1,30 @@
-package OAuth_JWT.oauth_jwt.security.handler;public class LoginSuccessHandler {
+package OAuth_JWT.oauth_jwt.security.handler;
+
+import OAuth_JWT.oauth_jwt.dto.AuthResponse;
+import OAuth_JWT.oauth_jwt.security.utils.TokenUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+    private final TokenUtils tokenUtils;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        new ObjectMapper().writeValue(response.getWriter(), new AuthResponse(tokenUtils.createToken(authentication)));
+    }
+
 }
