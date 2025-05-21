3783da1b37c38897d5a1f63d80c8e5fb1da315b0
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-21 15:12:07 +0900

    feat: 로그인 요청시 username과 password를이용하여 authenticationToken을 만들고 authenticationManger에게 건내주는 클래스

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/filter/LoginFilter.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/security/filter/LoginFilter.java
@@ -1,2 +1,48 @@
-package OAuth_JWT.oauth_jwt.security.filter;public class LoginFilter {
+package OAuth_JWT.oauth_jwt.security.filter;
+
+import OAuth_JWT.oauth_jwt.dto.LoginRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        if (!request.getMethod().equals("POST")) {
+            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+        }
+
+        LoginRequest loginRequest;
+        try {
+            loginRequest = new ObjectMapper().readValue(request.getInputStream(), LoginRequest.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
+
+        if (username == null) {
+            username = "";
+        }
+        if (password == null) {
+            password = "";
+        }
+
+        username = username.trim();
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        setDetails(request, authenticationToken);
+
+        return this.getAuthenticationManager().authenticate(authenticationToken);
+    }
+
 }
