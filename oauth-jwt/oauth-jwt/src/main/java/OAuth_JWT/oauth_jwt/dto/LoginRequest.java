ff8f4450b60118c589a783fdc956a4908989d674
jeongseonwoong <tjsdnd1219@naver.com>
2025-05-21 15:06:23 +0900

    feat: 로그인 요청을 위한 class

--- oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/dto/LoginRequest.java
+++ oauth-jwt/oauth-jwt/src/main/java/OAuth_JWT/oauth_jwt/dto/LoginRequest.java
@@ -1,2 +1,10 @@
-package OAuth_JWT.oauth_jwt.dto;public class LoginRequest {
+package OAuth_JWT.oauth_jwt.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LoginRequest {
+    String username;
+    String password;
 }
