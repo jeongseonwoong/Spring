d74469ac4e7ef7d58d783ce9d8dc157bdd0310f1
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-10 15:40:59 +0900

    feat: queue() => 큐 이름과 durable 설정, rabbitTemplate()=> rabbitMQ 사용 시 사용할 템플릿, ConnectionFactory => RabbitMQ와 연결할 때 사용되는 클래스 , SimpleMessageListenerContainr RabbitMq 큐 설정, 메시지 리스ㅌ너 설정 등, MessageListenerAdapter 컨슈머 클래스와 메서드 설정

---
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step0/MessageController.java
@@ -0,0 +1,7 @@
+package RabbitMQ.RabbitMQ.step0;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+}
---
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step0/Sender.java
@@ -0,0 +1,2 @@
+package RabbitMQ.RabbitMQ.step0;public class Sender {
+}
