016b57ded5ef74370dcf0886b7bddfb3cc63bfca
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-10 17:00:04 +0900

    feat: 요청으로 시간도 받아서 실행해보기

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step2/WorkQueueConsumer.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step2/WorkQueueConsumer.java
@@ -1,2 +1,26 @@
-package RabbitMQ.RabbitMQ.step2;public class WorkQueueConsumer {
+package RabbitMQ.RabbitMQ.step2;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WorkQueueConsumer {
+    public void workQueueTask(String message){
+        String[] messageParts = message.split("\\|");
+        String originMessage = messageParts[0];
+        int duration = Integer.parseInt(messageParts[1].trim());
+
+        System.out.println("Received: " + originMessage + "(duration: " + duration + "ms)");
+
+        try {
+            int second = duration /1000;
+            for (int num = 0; num < second; num++) {
+                Thread.sleep(1000);
+                System.out.print(".");
+            }
+            Thread.sleep(duration);
+        } catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("\nCompleted: " + originMessage);
+    }
 }
