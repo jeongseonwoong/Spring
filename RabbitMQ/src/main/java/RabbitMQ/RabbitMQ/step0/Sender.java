e4b486a31c5d402e0408d6fed8938e92290cc603
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-10 15:45:12 +0900

    feat: 생산자  클래스에서 rabbitTemplate으로 비동기 적으로 메시지 처리하도록 큐로 메시지 전송

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step0/Sender.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step0/Sender.java
@@ -1,2 +1,19 @@
-package RabbitMQ.RabbitMQ.step0;public class Sender {
+package RabbitMQ.RabbitMQ.step0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Sender {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void send(String message){
+        rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_NAME,message);
+        System.out.println("Sent: " + message);
+    }
+
+
 }
