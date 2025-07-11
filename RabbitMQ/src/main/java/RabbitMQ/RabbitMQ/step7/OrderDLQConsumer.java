5e3db94482a4b70b16c2ff70ab111bfca1430ac1
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-12 00:02:08 +0900

    feat: DLQ에 전송된 메시지를 변환하여 기존 큐에 다시 보낸다

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step7/OrderDLQConsumer.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step7/OrderDLQConsumer.java
@@ -1,2 +1,25 @@
-package RabbitMQ.RabbitMQ.step7;public class OrderDLQConsumer {
+package RabbitMQ.RabbitMQ.step7;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OrderDLQConsumer {
+    //DLQ를 처리하는 로직
+    private final RabbitTemplate rabbitTemplate;
+
+    @RabbitListener(queues = RabbitMQConfig7.DLQ)
+    public void process(String message){
+        System.out.println("DLQ Message Received: " + message);
+        try {
+            String fixMessage = "success";
+            rabbitTemplate.convertAndSend(RabbitMQConfig7.ORDER_EXCHANGE,"order.completed.shipping",fixMessage);
+            System.out.println("DLQ Message Sent: " + fixMessage);
+        }catch (Exception e){
+            System.out.println("DLQ Consumer Error" + e.getMessage());
+        }
+    }
 }
