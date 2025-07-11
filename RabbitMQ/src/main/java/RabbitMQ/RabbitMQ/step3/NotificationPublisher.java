a93dbfd30419c9577a31a6d6382c4f88ce96bd12
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 16:26:34 +0900

    feat: fanOut방식으로 익스체인지에게 메시지 넘겨주기

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step3/NotificationPublisher.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step3/NotificationPublisher.java
@@ -1,2 +1,19 @@
-package RabbitMQ.RabbitMQ.step3;public class NotificationPublisher {
+package RabbitMQ.RabbitMQ.step3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void publish(String message){
+        //FanoutExchange는 모든 연결된 키에 메시지를 넘겨주므로 라우팅 키가 따로 필요하지 않으므로 라우팅 키는 공백
+        rabbitTemplate.convertAndSend(RabbitMQConfig3.FANOUT_EXCHANGE,"",message);
+        System.out.println("Published Notification: " + message);
+    }
+
 }
