191b87eb6381185819660284ca1773d974e78c14
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 16:24:05 +0900

    feat: client로부터 온 메시지 웹소켓으로client로 전송

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step3/StompController.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step3/StompController.java
@@ -1,2 +1,22 @@
-package RabbitMQ.RabbitMQ.step3;public class StompController {
+package RabbitMQ.RabbitMQ.step3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class StompController {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/send")
+    public void sendMessage(NotificationMessage notificationMessage){
+        // 수신된 메시지를 브로드캐스팅
+        String message = notificationMessage.getMessage();
+        System.out.println("message = "+message);
+        //클라이언트에 메시지 바로 브로드캐스팅
+        simpMessagingTemplate.convertAndSend("/topic/notifications",message);
+    }
 }
