6be26cfbedc197efe400b71d11b57b1f606e242a
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 18:23:13 +0900

    feat: 웹소켓 기반 요청이 왔을 때 newsPublisher를 사용하여 exchange에 메시지 전달

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step4/NewsController.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step4/NewsController.java
@@ -1,2 +1,20 @@
-package RabbitMQ.RabbitMQ.step4;public class NewsController {
+package RabbitMQ.RabbitMQ.step4;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class NewsController {
+
+    private final NewsPublisher newsPublisher;
+
+    @MessageMapping("/subscribe")
+    public void handleSubscribe(@Header("newsType")String newsType){
+        System.out.println("newsType: " + newsType);
+        String newsMessage = newsPublisher.publish(newsType);
+        System.out.println("newsMessage: " + newsMessage);
+    }
 }
