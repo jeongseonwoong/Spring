a03042c7d57dbf68cba9b20cb345acf76f5fbab5
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 18:31:12 +0900

    feat: 큐로부터 온 메시지를 웹소켓을 이용해 특정 url로 전송

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step4/NewsSubscriber.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step4/NewsSubscriber.java
@@ -1,2 +1,27 @@
-package RabbitMQ.RabbitMQ.step4;public class NewsSubscriber {
+package RabbitMQ.RabbitMQ.step4;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class NewsSubscriber {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @RabbitListener(queues = RabbitMQConfig4.JAVA_QUEUE)
+    public void javaNews(String message){
+        simpMessagingTemplate.convertAndSend("/topic/java",message);
+    }
+
+    @RabbitListener(queues = RabbitMQConfig4.SPRING_QUEUE)
+    public void springNews(String message){
+        simpMessagingTemplate.convertAndSend("/topic/spring", message);
+    }
+
+    @RabbitListener(queues = RabbitMQConfig4.VUE_QUEUE)
+    public void vueNews(String message){
+        simpMessagingTemplate.convertAndSend("/topic/vue",message);
+    }
 }
