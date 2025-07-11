dfdca78833003b365e28d7d4df1808e960b57ca5
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-12 00:01:14 +0900

    feat: ORDER_EXCHANGE로 라우팅 키와 함께 메시지를 전송

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step7/OrderProducer.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step7/OrderProducer.java
@@ -1,2 +1,17 @@
-package RabbitMQ.RabbitMQ.step7;public class OrderProducer {
+package RabbitMQ.RabbitMQ.step7;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OrderProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sendShipping(String message){
+        rabbitTemplate.convertAndSend(RabbitMQConfig7.ORDER_EXCHANGE,"order.completed.shipping",message);
+        System.out.println("[주문 완료, 배송 지시 메시지 생성: " + message + "]");
+    }
 }
