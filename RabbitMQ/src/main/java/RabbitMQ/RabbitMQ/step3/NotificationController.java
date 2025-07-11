01d8a4a777c00b3ffce23603a83cf541bb625a44
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 16:25:17 +0900

    feat: http 방식으로 받은 메시지를 구독하고 있는 모든 subscriber에게 전송

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step3/NotificationController.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step3/NotificationController.java
@@ -1,2 +1,22 @@
-package RabbitMQ.RabbitMQ.step3;public class NotificationController {
+package RabbitMQ.RabbitMQ.step3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationPublisher publisher;
+
+    @PostMapping
+    public String sendNotification(@RequestBody String message){
+        publisher.publish(message);
+        return "Notification Send: " + message + "\n";
+    }
+
 }
