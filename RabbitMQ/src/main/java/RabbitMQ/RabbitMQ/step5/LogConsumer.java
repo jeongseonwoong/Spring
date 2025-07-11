6dd01ffb3e174d2d887410affa36ec7983dd69b3
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 19:07:16 +0900

    feat: 익스체인지로부터 특정 큐에 메시지가 들어오면 실행되는 로직정의

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step5/LogConsumer.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step5/LogConsumer.java
@@ -1,2 +1,23 @@
-package RabbitMQ.RabbitMQ.step5;public class LogConsumer {
+package RabbitMQ.RabbitMQ.step5;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LogConsumer {
+
+    @RabbitListener(queues = RabbitMQConfig5.ERROR_QUEUE)
+    public void consumeError(String message){
+        System.out.println("Error를 받음: " + message );
+    }
+
+    @RabbitListener(queues = RabbitMQConfig5.WARN_QUEUE)
+    public void consumeWarn(String message){
+        System.out.println("Warn를 받음: " + message );
+    }
+
+    @RabbitListener(queues = RabbitMQConfig5.INFO_QUEUE)
+    public void consumeInfo(String message){
+        System.out.println("Info를 받음: " + message );
+    }
 }
