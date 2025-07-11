60b255bed27752e18e6f7b16c163b0238e80a20c
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 19:06:15 +0900

    feat: 예외에 따른 routingkey작성하여 publisher 호출하는 클래스

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step5/CustomExceptionHandler.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step5/CustomExceptionHandler.java
@@ -1,2 +1,36 @@
-package RabbitMQ.RabbitMQ.step5;public class CustomExceptionHandler {
+package RabbitMQ.RabbitMQ.step5;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Component
+@RequiredArgsConstructor
+public class CustomExceptionHandler {
+
+    private final LogPublisher logPublisher;
+
+    //에러나 로그 처리
+    public void handleException(Exception e){
+        String message = e.getMessage();
+
+        String routingKey;
+
+        if(e instanceof NullPointerException){
+            routingKey = "error";
+        }else if(e instanceof  IllegalArgumentException){
+            routingKey = "warn";
+        }else{
+            routingKey = "error";
+        }
+
+        logPublisher.publish(routingKey,"Exception이 발생했음: " + message);
+    }
+
+    //메시지 처리
+    public void handleMessage(String message){
+        String routingKey = "info";
+        logPublisher.publish(routingKey,"Info Log: " + message);
+    }
+
 }
