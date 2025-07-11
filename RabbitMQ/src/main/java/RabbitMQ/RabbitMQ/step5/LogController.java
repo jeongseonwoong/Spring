77175f9bbb6c90f3d1b26286e83eb77c2a244990
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 19:07:50 +0900

    feat: 요청에 따라 예외를 발생시킨 후 예외 핸들러로 전달

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step5/LogController.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step5/LogController.java
@@ -1,2 +1,40 @@
-package RabbitMQ.RabbitMQ.step5;public class LogController {
+package RabbitMQ.RabbitMQ.step5;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/logs")
+public class LogController {
+
+    private final CustomExceptionHandler customExceptionHandler;
+
+    @GetMapping("/error")
+    public ResponseEntity<String> errorAPI(){
+        try{
+            String value = null;
+            value.getBytes(); //null pointer
+        }catch (Exception e){
+            customExceptionHandler.handleException(e);
+        }
+        return ResponseEntity.ok().body("nullpointer Exception 처리");
+    }
+
+    @GetMapping("/warn")
+    public ResponseEntity<String> warnAPI(){
+        try{
+            throw new IllegalAccessException("invalid argument입니다.");
+        } catch (IllegalAccessException e) {
+            customExceptionHandler.handleException(e);
+        }
+        return ResponseEntity.ok().body("IllegalArgument Exception 처리");
+    }
+
+    @PostMapping("/info")
+    public ResponseEntity<String> infoAPI(@RequestBody String message){
+        customExceptionHandler.handleMessage(message);
+        return ResponseEntity.ok().body("info log 발송 처리");
+    }
 }
