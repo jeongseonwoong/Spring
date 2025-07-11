9359663dd37653a78c54c846f8397a51ad797351
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 19:08:25 +0900

    feat: 익스체인지에서 라우팅 키에 따라 메시지를 전달하도록 설정

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step5/LogPublisher.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step5/LogPublisher.java
@@ -1,2 +1,18 @@
-package RabbitMQ.RabbitMQ.step5;public class LogPublisher {
+package RabbitMQ.RabbitMQ.step5;
+
+import RabbitMQ.RabbitMQ.step4.RabbitMQConfig4;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LogPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void publish(String routingKey,String message){
+        rabbitTemplate.convertAndSend(RabbitMQConfig5.DIRECT_EXCHANGE,routingKey,message);
+        System.out.println("message published: "+ routingKey + " " + message);
+    }
 }
