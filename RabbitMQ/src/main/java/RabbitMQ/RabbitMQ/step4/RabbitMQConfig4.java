4af761d5c0f947bf6581e9f07837421e67168001
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 18:26:34 +0900

    feat: 큐를 여러개 만들고 모두 하나의 exchange에 연결

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step4/RabbitMQConfig4.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step4/RabbitMQConfig4.java
@@ -1,2 +1,48 @@
-package RabbitMQ.RabbitMQ.step4;public class RabbitMQConfig {
+package RabbitMQ.RabbitMQ.step4;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig4 {
+
+    public static final String FANOUT_EXCHANGE_FOR_NEWS = "newsExchange";
+
+    public static final String JAVA_QUEUE = "javaQueue";
+    public static final String SPRING_QUEUE = "springQueue";
+    public static final String VUE_QUEUE = "vueQueue";
+
+    @Bean
+    public Queue javaQueue(){return new Queue(JAVA_QUEUE,false);}
+
+    @Bean
+    public Queue springQueue(){return new Queue(SPRING_QUEUE,false);}
+
+    @Bean
+    public Queue vueQueue(){return new Queue(VUE_QUEUE,false);}
+
+    @Bean
+    public FanoutExchange fanoutExchange(){
+        return new FanoutExchange(FANOUT_EXCHANGE_FOR_NEWS);
+    }
+
+    @Bean
+    public Binding javaBinding(@Qualifier("javaQueue") Queue javaQueue, FanoutExchange fanoutExchange){
+        return BindingBuilder.bind(javaQueue).to(fanoutExchange);
+    }
+
+    @Bean
+    public Binding springBinding(@Qualifier("springQueue") Queue springQueue, FanoutExchange fanoutExchange){
+        return BindingBuilder.bind(springQueue).to(fanoutExchange);
+    }
+
+    @Bean
+    public Binding vueBinding(@Qualifier("vueQueue") Queue vueQueue, FanoutExchange fanoutExchange){
+        return BindingBuilder.bind(vueQueue).to(fanoutExchange);
+    }
 }
