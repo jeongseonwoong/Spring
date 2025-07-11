fb126b0c5911d664c40da69eb48f10ac214bd4b5
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 16:21:54 +0900

    feat: 메시지 주고받기 위한 DTO

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step3/NotificationMessage.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step3/NotificationMessage.java
@@ -1,2 +1,14 @@
-package RabbitMQ.RabbitMQ.step3;public class NotificationMessage {
+package RabbitMQ.RabbitMQ.step3;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationMessage {
+    private String message;
 }
