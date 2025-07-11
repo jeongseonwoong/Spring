8b873dcb0288396b6bd78b4b2eda5f7b81a6d513
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-11 18:19:51 +0900

    feat: 확인용 컨트롤러

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step4/HomeController.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step4/HomeController.java
@@ -1,2 +1,21 @@
-package RabbitMQ.RabbitMQ.step4;public class HomeController {
+package RabbitMQ.RabbitMQ.step4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(Model model){
+        model.addAttribute("message","Welcome to RabbitMQ Sample!");
+        return "home";
+    }
+
+    @GetMapping("/news")
+    public String news(Model model){
+        model.addAttribute("message","Welcome to RabbitMQ News Sample!");
+        return "news";
+    }
 }
