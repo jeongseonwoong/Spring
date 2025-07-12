76a8cc5b7894a6c64c28d7fbde84c9c962bdd980
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-12 09:49:23 +0900

    feat: DLQ에 들어온 메시지 고쳐서원래 큐로 배니주기

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step8/OrderDeadLetterRetry.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step8/OrderDeadLetterRetry.java
@@ -1,6 +1,5 @@
 package RabbitMQ.RabbitMQ.step8;
 
-import RabbitMQ.RabbitMQ.step7.RabbitMQConfig7;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
