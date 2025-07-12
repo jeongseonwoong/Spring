1b5a2c8c20bf008631fc479b926a26c60ff732b8
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-12 09:50:23 +0900

    feat:Retry config를 이용해서 재시도 처리하도록 변경

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step8/OrderConsumer.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step8/OrderConsumer.java
@@ -1,53 +1,40 @@
-package RabbitMQ.RabbitMQ.step7;
+package RabbitMQ.RabbitMQ.step8;
 
-import com.rabbitmq.client.Channel;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 public class OrderConsumer {
-    private static final int MAX_RETRIES = 3;
-    private int retryCount = 0;
 
-    /**
-     * containerFactory는 RabbitMQManualConfig7에서 설정한 rabbitListenerContainerFactory()
-     * @param message 메시지
-     * @param channel Ack,Nack 처리할 때 필요함.
-     * @param tag 메시지가 가지고 있는 고유번호
-     */
-    @RabbitListener(queues = RabbitMQConfig7.ORDER_COMPLETED_QUEUE, containerFactory = "rabbitListenerContainerFactory")
-    public void processOrder(String message, Channel channel, @Header("amqp_deliveryTag") long tag){
-        try {
-            //RabbitMQ의 메시지 소비는 **"메시지 1건 단위로 처리 후 실패 시 재전송"**되는 구조
-            //예외 발생 시 메시지를 다시 큐에 requeue 후 재전송 시도.
-            if("fail".equalsIgnoreCase(message)){
-                if(retryCount < MAX_RETRIES){
-                    System.out.println("Fail & Retry: " + retryCount);
-                    retryCount++;
-                    throw new RuntimeException(message);
-                }else{
-                    System.out.println("최대 횟수 초과, DLQ 이동시킴");
-                    retryCount = 0;
-                    channel.basicNack(tag,false,false);
-                    return;
-                }
-            }
-            //성공 처리
-            System.out.println("성공: " + message);
-            channel.basicAck(tag,false);
-            retryCount=0;
-        }catch (Exception e){
-            System.out.println("error 발생: "+ e.getMessage());
-            try{
-                //실패 시 basicReject 재처리 정송
-                channel.basicReject(tag,true);
-            }catch (IOException e1){
-                System.out.println("fail & reject message : " + e1.getMessage());
-            }
-        }
+    private final RabbitTemplate rabbitTemplate;
+    private final RetryTemplate retryTemplate;
+
+    public OrderConsumer(RabbitTemplate rabbitTemplate, RetryTemplate retryTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.retryTemplate = retryTemplate;
     }
 
+    @RabbitListener(queues = RabbitMQConfig8.ORDER_COMPLETED_QUEUE)
+    public void consume(String message) {
+        retryTemplate.execute(context -> {
+            try {
+                System.out.println("# 리시브 메시지 : " + message + " # retry : " + context.getRetryCount());
+                // 실패 조건
+                if ("fail".equalsIgnoreCase(message)) {
+                    throw new RuntimeException(message);
+                }
+                System.out.println("# 메시지 처리 성공 " + message);
+            } catch (Exception e) {
+                if (context.getRetryCount() >= 2) {
+                    rabbitTemplate.convertAndSend(RabbitMQConfig8.ORDER_TOPIC_DLX,
+                            RabbitMQConfig8.DEAD_LETTER_ROUTING_KEY, message);
+                } else {
+                    throw e;
+                }
+            }
+            return null;
+        });
+    }
 }
