e97d196c3ec9132394943e54df5aae2ee201f533
jeongseonwoong <tjsdnd1219@naver.com>
2025-07-12 09:48:47 +0900

    feat: DLQ에 들어온 메시지 고쳐서원래 큐로 배니주기

--- RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step8/OrderDeadLetterRetry.java
+++ RabbitMQ/src/main/java/RabbitMQ/RabbitMQ/step8/OrderDeadLetterRetry.java
@@ -8,17 +8,23 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class OrderDLQConsumer {
+public class OrderDeadLetterRetry {
     //DLQ를 처리하는 로직
     private final RabbitTemplate rabbitTemplate;
 
-    @RabbitListener(queues = RabbitMQConfig7.DLQ)
+    @RabbitListener(queues = RabbitMQConfig8.DLQ)
     public void process(String message){
         System.out.println("DLQ Message Received: " + message);
         try {
-            String fixMessage = "success";
-            rabbitTemplate.convertAndSend(RabbitMQConfig7.ORDER_EXCHANGE,"order.completed.shipping",fixMessage);
-            System.out.println("DLQ Message Sent: " + fixMessage);
+            if("fail".equalsIgnoreCase(message)){
+                message = "success";
+                System.out.println("Message fixed:" + message);
+            }else{
+                System.out.println("Message already fixed: " + message);
+                return;
+            }
+            rabbitTemplate.convertAndSend(RabbitMQConfig8.ORDER_TOPIC_EXCHANGE,"order.completed",message);
+            System.out.println("DLQ Message Sent: " + message);
         }catch (Exception e){
             System.out.println("DLQ Consumer Error" + e.getMessage());
         }
